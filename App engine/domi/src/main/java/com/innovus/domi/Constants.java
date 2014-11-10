4e4c164131e2957e70c90aec8cc976d6afdf087c
innovusjuanpantoja <juancarlospantoja@hotmail.com>
2014-11-10 13:46:29 -0500

    app movil
    una app android alias doomi con la configuracion de consumir los web
    services

--- App engine/domi/src/main/java/com/innovus/domi/Constants.java
+++ App engine/domi/src/main/java/com/innovus/domi/Constants.java
@@ -7,7 +7,7 @@ import com.google.api.server.spi.Constant;
  */
 public class Constants {
   public static final String WEB_CLIENT_ID = "204916157214-1hho3fgafmt30l9kt7rljls1qttbeb3n.apps.googleusercontent.com";
-  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String ANDROID_CLIENT_ID = "204916157214-rl2danb6k5nbv0h4heonvmqia8q1ff25.apps.googleusercontent.com";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
   public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
