95cc2d21df84f0d622a828e9e92d8bf150eef1c0
Jorge Luis Viveros <jorge_luis91x@hotmail.com>
2015-02-15 13:51:41 -0500

    sofity

--- doomi android/Spofity/app/src/main/java/com/innovus/spofity/MainActivity.java
+++ doomi android/Spofity/app/src/main/java/com/innovus/spofity/MainActivity.java
@@ -4,21 +4,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MyActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity {
+    public EditText editText;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -28,9 +33,19 @@ public class MyActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
+    }
+    public void onclick(View v){
+        editText= (EditText)findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        textView.setText("Hola "+editText.getText().toString()) ;
+
     }
 }
